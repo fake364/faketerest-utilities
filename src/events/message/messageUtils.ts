343c47b4c8b337c6e9ec2b5645b442a7854a3f29
fake364 <serebreanu.v@gmail.com>
2022-11-18 17:38:04 +0200

    added key util

--- src/events/message/messageUtils.ts
+++ src/events/message/messageUtils.ts
@@ -3,6 +3,13 @@ import EVENT_TYPE from "../types";
 const createConversationId = (...ids: number[]) =>
   `${EVENT_TYPE.MESSAGE}:${ids.sort((a, b) => a - b).join(":")}`;
 
-const MessageUtils = { createConversationId };
+const getParticipants = (dialogKey: string, myId: number) =>
+  dialogKey
+    .split(":")
+    .slice(1)
+    .filter((participant) => participant !== String(myId))
+    .map(Number);
+
+const MessageUtils = { createConversationId, getParticipants };
 
 export default MessageUtils;
